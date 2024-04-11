8b7e5065754db639ad36946bb513bbdcfd36da9c
FallBackITA27 <diegomassanova4@gmail.com>
2024-04-11 17:20:00 +0200

    im just a moron in all of this

--- mkwpp-parser/script.js
+++ mkwpp-parser/script.js
@@ -1,3 +1,59 @@
+let data = {
+    submissions: [],
+};
+
+let constants = {
+    months: {
+        "ja": "01",
+        "f": "02",
+        "mar": "03",
+        "ap": "04",
+        "may": "05",
+        "jun": "06",
+        "jul": "07",
+        "au": "08",
+        "s": "09",
+        "o": "10",
+        "n": "11",
+        "d": "12",
+    },
+
+    tracks: {
+        "l": 0,
+        "mm": 1,
+        "mg": 2,
+        "t": 3,
+        "mc": 4,
+        "cm": 5,
+        "dk": 6,
+        "w": 7,
+        "dc": 8,
+        "k": 9,
+        "mt": 10,
+        "g": 11,
+        "dd": 12,
+        "mh": 13,
+        "b": 14,
+        "rr": 15,
+        "rpb": 16,
+        "ry": 17,
+        "rg": 18,
+        "rmr": 19,
+        "rsl": 20,
+        "rsg": 21,
+        "rds": 22,
+        "rw": 23,
+        "rdh": 24,
+        "rbc3": 25,
+        "rdkj": 26,
+        "rmc": 27,
+        "rmc3": 28,
+        "rpb": 29,
+        "rdkm": 30,
+        "rbc": 31,
+    }
+}
+
 function writeToOutput(d) {
     if (d == null) return;
     let out = document.createElement("p");
@@ -9,169 +65,118 @@ function resetOutput() {
     document.getElementById("output").innerHTML = "";
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    let data = {submissions:[]};
-    let constants = {
-        months: {
-            "ja": "01",
-            "f": "02",
-            "mar": "03",
-            "ap": "04",
-            "may": "05",
-            "jun": "06",
-            "jul": "07",
-            "au": "08",
-            "s": "09",
-            "o": "10",
-            "n": "11",
-            "d": "12",
-        },
-        tracks: {
-            "l": 0,
-            "mm": 1,
-            "mg": 2,
-            "t": 3,
-            "mc": 4,
-            "cm": 5,
-            "dk": 6,
-            "w": 7,
-            "dc": 8,
-            "k": 9,
-            "mt": 10,
-            "g": 11,
-            "dd": 12,
-            "mh": 13,
-            "b": 14,
-            "rr": 15,
-            "rpb": 16,
-            "ry": 17,
-            "rg": 18,
-            "rmr": 19,
-            "rsl": 20,
-            "rsg": 21,
-            "rds": 22,
-            "rw": 23,
-            "rdh": 24,
-            "rbc3": 25,
-            "rdkj": 26,
-            "rmc": 27,
-            "rmc3": 28,
-            "rpb": 29,
-            "rdkm": 30,
-            "rbc": 31,
-        }
-    };
-    console.log(document.getElementById("readInput"));
-    document.getElementById("readInput").addEventListener("click", async function() {
-        console.log("Started");
-        let parserData = document.getElementById("inputTextArea").value.split("\n").filter(r=>r !== "");
-        let currentSubmission = {skip:true};
-        let skipToNextSubmission = false;
-        for (let line of parserData) {
-            let lowercaseLine = line.toLowerCase();
-            if (lowercaseLine.startsWith("date")) {
-                skipToNextSubmission = false
-                data.submissions.push(currentSubmission);
-                let year;
-                let month;
-                let date;
-                let keywords = lowercaseLine.split(" ").filter(r=>r !== " ");
-                if (keywords.length > 4) {
-                    skipToNextSubmission = true;
-                    currentSubmission.err = true;
-                    currentSubmission.errString = "Cannot parse date";
-                };
-                for (let keyword of keywords.slice(1, keywords.length)) {
-                    let kw = keyword.replace(/,/g, "");
-                    if (kw.length === 4) {
-                        year = kw;
-                        continue;
-                    }
-                    let _break = false;
-                    for (let abbr of Object.keys(constants.months)) if (kw.startsWith(abbr)) {
-                        month = constants.months[abbr];
-                        _break = true;
-                        break;
-                    }
-                    if (_break) continue;
-                    let kwfiltered = kw.replace(/nth/g, "").replace(/rd/g, "").replace(/nd/g, "").replace(/st/g, "");
-                    date = kwfiltered.padStart(2,"0")
-                };
-                currentSubmission = {
-                    name: "",
-                    date: `${year}-${month}-${date}`,
-                    flapCatch: false,
-                    times: [],
-                };
-            } else if (lowercaseLine.startsWith("name")) {
-                let keywords = lowercaseLine.split(" ").filter(r=>r !== " ");
-                currentSubmission.name = keywords.slice(1,keywords.length).join(" ");
-            }
-            if (skipToNextSubmission) continue;
-
-            if (lowercaseLine.split(" ").filter(r=>r !== "").length === 1 && (lowercaseLine.startsWith("f") || lowercaseLine.startsWith("l"))) {
-                currentSubmission.flapCatch = true
-                continue;
+document.getElementById("readInput").addEventListener("click", async function() {
+    console.log("Started");
+    let parserData = document.getElementById("inputTextArea").value.split("\n").filter(r=>r !== "");
+    let currentSubmission = {skip:true};
+    let skipToNextSubmission = false;
+    for (let line of parserData) {
+        let lowercaseLine = line.toLowerCase();
+        if (lowercaseLine.startsWith("date")) {
+            skipToNextSubmission = false
+            data.submissions.push(currentSubmission);
+            let year;
+            let month;
+            let date;
+            let keywords = lowercaseLine.split(" ").filter(r=>r !== " ");
+            if (keywords.length > 4) {
+                skipToNextSubmission = true;
+                currentSubmission.err = true;
+                currentSubmission.errString = "Cannot parse date";
             };
-
-            let track = -1;
-            let data = lowercaseLine.split(" ").filter(r=>r!=="");
-
-            for (let abbr of Object.keys(constants.tracks)) if (data[0].startsWith(abbr)) {
-                track = constants.tracks[abbr];
-                break;
-            }
-            data.slice(1,data.length);
-
-            if (track === -1) {
-                // report line skip
-                continue
-            }
-
-            let nosc = false;
-            let flap = currentSubmission.flapCatch;
-
-            let i = 0;
-            let remove = [];
-            for (let token of data) {
-                if (token.startsWith("n")) {
-                    nosc = true;
-                    remove.push(i);
+            for (let keyword of keywords.slice(1, keywords.length)) {
+                let kw = keyword.replace(/,/g, "");
+                if (kw.length === 4) {
+                    year = kw;
+                    continue;
                 }
-                if (token.startsWith("f") || token.startsWith("l")) {
-                    flap = true;
-                    remove.push(i);
+                let _break = false;
+                for (let abbr of Object.keys(constants.months)) if (kw.startsWith(abbr)) {
+                    month = constants.months[abbr];
+                    _break = true;
+                    break;
                 }
-                i++;
-            }
-            for (let x of remove) data[x] = "";
-            data = data.filter(r=>r!=="");
-
-            if (lowercaseLine.contains("/") || lowercaseLine.contains("\\")) {
-                let data1 = [];
-                let data2 = [];
-                let x = false;
-                for (let token of data) {
-                    if (token === "/" || token === "\\") {
-                        x = true;
-                        continue;
-                    }
-                    if (x) {
-                        data2.push(token);
-                    } else {
-                        data1.push(token);
-                    }
-                }
-                if (data1.length !== 0) data.submissions.times.push(handleTime(data1,track,nosc,false));
-                if (data2.length !== 0) data.submissions.times.push(handleTime(data2,track,nosc,true));
-                continue;
-            }
-
-            data.submissions.times.push(handleTime(data,track,nosc,flap));
+                if (_break) continue;
+                let kwfiltered = kw.replace(/nth/g, "").replace(/rd/g, "").replace(/nd/g, "").replace(/st/g, "");
+                date = kwfiltered.padStart(2,"0")
+            };
+            currentSubmission = {
+                name: "",
+                date: `${year}-${month}-${date}`,
+                flapCatch: false,
+                times: [],
+            };
+        } else if (lowercaseLine.startsWith("name")) {
+            let keywords = lowercaseLine.split(" ").filter(r=>r !== " ");
+            currentSubmission.name = keywords.slice(1,keywords.length).join(" ");
         }
-        data.submissions.push(currentSubmission);
-        console.log("Finished");
-    });
+        if (skipToNextSubmission) continue;
+
+        if (lowercaseLine.split(" ").filter(r=>r !== "").length === 1 && (lowercaseLine.startsWith("f") || lowercaseLine.startsWith("l"))) {
+            currentSubmission.flapCatch = true
+            continue;
+        };
+
+        let track = -1;
+        let trackData = lowercaseLine.split(" ").filter(r=>r!=="");
+
+        for (let abbr of Object.keys(constants.tracks)) if (trackData[0].startsWith(abbr)) {
+            track = constants.tracks[abbr];
+            break;
+        }
+        trackData.slice(1,trackData.length);
+
+        if (track === -1) {
+            // report line skip
+            continue
+        }
+
+        let nosc = false;
+        let flap = currentSubmission.flapCatch;
+
+        let i = 0;
+        let remove = [];
+        for (let token of trackData) {
+            if (token.startsWith("n")) {
+                nosc = true;
+                remove.push(i);
+            }
+            if (token.startsWith("f") || token.startsWith("l")) {
+                flap = true;
+                remove.push(i);
+            }
+            i++;
+        }
+        for (let x of remove) trackData[x] = "";
+        trackData = trackData.filter(r=>r!=="");
+
+        if (lowercaseLine.contains("/") || lowercaseLine.contains("\\")) {
+            let trackData1 = [];
+            let trackData2 = [];
+            let x = false;
+            for (let token of trackData) {
+                if (token === "/" || token === "\\") {
+                    x = true;
+                    continue;
+                }
+                if (x) {
+                    trackData2.push(token);
+                } else {
+                    trackData1.push(token);
+                }
+            }
+            if (trackData1.length !== 0) trackData.submissions.times.push(handleTime(trackData1,track,nosc,false));
+            if (trackData2.length !== 0) trackData.submissions.times.push(handleTime(trackData2,track,nosc,true));
+            continue;
+        }
+
+        data.submissions.times.push(handleTime(trackData,track,nosc,flap));
+    }
+    data.submissions.push(currentSubmission);
+    console.log("Finished");
+});
+
+document.addEventListener("DOMContentLoaded", function() {
 
     // fetch("https://www.mariokart64.com/mkw/profile.php").then(r=>r.text()).then(r=>{
     //     let profileDocument = new DOMParser().parseFromString(r);
