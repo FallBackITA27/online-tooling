c081d423e7808245bed0fd1f32358d8e5cbe073e
FallBackITA27 <diegomassanova4@gmail.com>
2024-04-13 00:08:12 +0200

    THE ONE TIME I NEED `for ... in` INSTEAD OF `for ... of`

--- mkwpp-ctgp-doublecheck/script.js
+++ mkwpp-ctgp-doublecheck/script.js
@@ -131,7 +131,7 @@ document.getElementById("startChecker").addEventListener("click", async function
                 normal: parseMKWPPTable(profileDocument.getElementsByClassName("k")[0])
             }
             for (let trackUnr of data.unrestricted["3lap"]) for (let trackNor of data.normal["3lap"]) if (data.unrestricted["3lap"][trackUnr] === data.normal["3lap"][trackNor]) delete data.unrestricted["3lap"][trackUnr];
-            for (let trackUnr of data.unrestricted.flap) for (let trackNor of data.normal.flap) if (data.unrestricted.flap[trackUnr] === data.normal.flap[trackNor]) delete data.unrestricted.flap[trackUnr];
+            for (let trackUnr in data.unrestricted.flap) for (let trackNor in data.normal.flap) if (data.unrestricted.flap[trackUnr] === data.normal.flap[trackNor]) delete data.unrestricted.flap[trackUnr];
         });
         let awaiting = [];
         for (let id of ctgpLinks[ppid]) {
