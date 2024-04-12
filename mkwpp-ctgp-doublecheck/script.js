5744beab6a2c1ff0b1cbcc1359a589161e78dc75
FallBackITA27 <diegomassanova4@gmail.com>
2024-04-12 20:51:37 +0200

    new tool test

---
+++ mkwpp-ctgp-doublecheck/script.js
@@ -0,0 +1,82 @@
+const ctgpLinks = {
+    1662: ["1F7B7D3331A3A008"] // FalB
+}
+
+/*
+This is a timesheet
+{
+    playerPPID: {
+        Normal: {
+            3lap: {
+                trackId: {
+                    time: timeInMilliseconds
+                }
+                ...
+            },
+            flap: {
+                trackId: {
+                    time: timeInMilliseconds
+                }
+                ...
+            },
+        },
+        Unrestricted: {
+            3lap: {
+                trackId: {
+                    time: timeInMilliseconds
+                }
+                ...
+            },
+            flap: {
+                trackId: {
+                    time: timeInMilliseconds
+                }
+                ...
+            },
+        }
+    }
+}
+*/
+
+function writeToOutput(d) {
+    if (d == null) return;
+    let out = document.createElement("p");
+    out.innerHTML = d;
+    document.getElementById("output").append(out);
+}
+
+function resetOutput() {
+    document.getElementById("output").innerHTML = "";
+}
+
+document.getElementById("startChecker").addEventListener("click", async function() {
+    for (let ppid in ctgpLinks) {
+        let chadsoftTimeSheet = {};
+        let mkwppTimesheetRequest = parseTimesheetMKWPP(`https://corsproxy.io/?https://www.mariokart64.com/mkw/profile.php?pid=${i}`);
+
+        let mkwppTimesheet = await mkwppTimesheetRequest;
+    }
+});
+
+async function parseTimesheetMKWPP(link) {
+
+}
+
+async function parseTimesheetCTGP(link) {
+    
+}
+
+async function writeObservedPlayers() {
+    fetch("https://corsproxy.io/?https://www.mariokart64.com/mkw/profile.php").then(r=>r.text()).then(r=>{
+        let profileDocument = new DOMParser().parseFromString(r, "text/html");
+        let playerList = profileDocument.getElementsByClassName("playerslist")[0].children[0];
+        for (let row of playerList.children) {
+            let starterData = row.children[0].children[0];
+            if (row.children[0].innerHTML === "Name") continue;
+            if (!Object.keys(ctgpLinks).includes(starterData.href.split("=")[1])) continue;
+            document.getElementById("info").appendChild(starterData);
+        }
+        document.getElementById("startChecker").disabled = "";
+    });
+}
+writeObservedPlayers();
