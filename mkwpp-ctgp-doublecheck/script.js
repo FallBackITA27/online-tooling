a0b9551716ee11c11ad8706305de1578c9cecc50
FallBackITA27 <diegomassanova4@gmail.com>
2024-04-12 23:19:37 +0200

    I'm a genuine moron

--- mkwpp-ctgp-doublecheck/script.js
+++ mkwpp-ctgp-doublecheck/script.js
@@ -136,14 +136,11 @@ document.getElementById("startChecker").addEventListener("click", async function
         let awaiting = [];
         for (let id of ctgpLinks[ppid]) {
             awaiting.push(fetch(`https://tt.chadsoft.co.uk/players/${id.slice(0,2)}/${id.slice(2)}.json`).then(r=>r.json()).then(r=>{
-                for (let ghost in r.ghosts) {
+                for (let ghost of r.ghosts) {
                     if (ghost["200cc"]) continue;
                     let track = ctgpIdsToTrackNums[ghost.trackId];
-                    console.log(ghost);
                     if (track == null || track == undefined) continue;
-                    console.log(`debug 1`);
                     if (ghost.categoryId == 3 || ghost.categoryId == 5) continue;
-                    console.log(`debug 2`);
                     let category;
                     if (ghost.categoryId == null || ghost.categoryId == undefined || ghost.categoryId == 2 || ghost.categoryId == 0) {
                         category = "normal";
