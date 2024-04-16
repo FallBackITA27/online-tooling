5888dfadc5eefc2a90cac858e4102cf946179413
FallBackITA27 <diegomassanova4@gmail.com>
2024-04-16 15:41:29 +0200

    more selection stuff

--- sy-clan/player-timesheet/script.js
+++ sy-clan/player-timesheet/script.js
@@ -1,5 +1,21 @@
+let selectionData = {
+    playerData: fetch(`../assets/falb.json`).then(r=>r.json()),
+    category: "nosc",
+};
+
 document.getElementById("playerPicker").addEventListener("change", function(e) {
-    fetch(`../assets/${e.target.value}.json`).then(r=>r.json()).then(r=>{
-        console.log(r);
-    });
+    selectionData.playerData = fetch(`../assets/${e.target.value}.json`).then(r=>r.json());
 });
+
+document.getElementById("categoryPicker").addEventListener("change", function(e) {
+    selectionData.category = e.target.value;
+});
+
+function formatMsToTime(i32) {
+    let mins = Math.trunc(i32 / 60000);
+    i32 %= 60000;
+    let sec = Math.trunc(i32 / 1000);
+    i32 %= 1000;
+    let ret = `${mins.toString().padStart(2, "0")}:${sec.toString().padStart(2, "0")}.${Math.trunc(i32).toString().padStart(3, "0")}`;
+    return ret;
+}
