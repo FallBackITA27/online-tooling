def995fcc7545cf678399dac31b796b84293af40
FallBackITA27 <diegomassanova4@gmail.com>
2024-04-16 19:00:09 +0200

    bugfix

--- sy-clan/total-timesheet/script.js
+++ sy-clan/total-timesheet/script.js
@@ -51,7 +51,7 @@ async function start() {
         let total = {unr: {}, nosc: {}};
         for (let filename of r) {
             let player = await fetch(`../assets/${filename}.json`).then(r=>r.json());
-            for (let track of player) {
+            for (let track in player) {
                 for (let category of track) {
                     let timeData = player[track][category];
                     if (total[category][track] == null || total[category][track] == undefined) {
