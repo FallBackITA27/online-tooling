344df44d2b2c9f8d29f7b65f41129c419ba02eaa
FallBackITA27 <diegomassanova4@gmail.com>
2024-04-16 15:59:55 +0200

    em push

--- sy-clan/player-timesheet/script.js
+++ sy-clan/player-timesheet/script.js
@@ -1,3 +1,38 @@
+const trackNumToName = {
+    0: "Luigi Circuit",
+    1: "Moo Moo Meadows",
+    2: "Mushroom Gorge",
+    3: "Toad's Factory",
+    4: "Mario Circuit",
+    5: "Coconut Mall",
+    6: "DK Snowboard Cross",
+    7: "Wario's Gold Mine",
+    8: "Daisy Circuit",
+    9: "Koopa Cape",
+    10: "Maple Treeway",
+    11: "Grumble Volcano",
+    12: "Dry Dry Ruins",
+    13: "Moonview Highway",
+    14: "Bowser's Castle",
+    15: "Rainbow Road",
+    16: "GCN Peach Beach",
+    17: "DS Yoshi Falls",
+    18: "SNES Ghost Valley 2",
+    19: "N64 Mario Raceway",
+    20: "N64 Sherbet Land",
+    21: "GBA Shy Guy Beach",
+    22: "DS Delfino Square",
+    23: "GCN Waluigi Stadium",
+    24: "DS Desert Hills",
+    25: "GBA Bowser Castle 3",
+    26: "N64 DK's Jungle Parkway",
+    27: "GCN Mario Circuit",
+    28: "SNES Mario Circuit 3",
+    29: "DS Peach Gardens",
+    30: "GCN DK Mountain",
+    31: "N64 Bowser's Castle",
+}
+
 let selectionData = {
     playerData: fetch(`../assets/falb.json`).then(r=>r.json()),
     category: "nosc",
@@ -16,11 +51,8 @@ document.getElementById("categoryPicker").addEventListener("change", function(e)
 async function updateDisplay() {
     await selectionData.playerData.then(r=>{
         for (let i = 0; i < 32; i++) {
-            console.log(r);
-            console.log(i.toString());
-            console.log(selectionData.category);
             let data = r[i.toString()][selectionData.category];
-            console.log(data);
+            let trackName = trackNumToName[i];
         }
     });
 }
