d4b22eeb80b261489731a757256bce3de44f8719
FallBackITA27 <diegomassanova4@gmail.com>
2024-05-09 20:48:58 +0200

    map :eyes:

---
+++ gtav-utils/script.js
@@ -0,0 +1,45 @@
+var map = L.map('map', {
+    center: [0, 0],
+    zoom: 0,
+    inertia: true,
+    worldCopyJump: false,
+});
+
+let tileLayerData = {
+    mainMap: {
+        render: {
+            oceanColor: "#384950",
+            tileLayerOpts: {
+                maxZoom: 7,
+                minZoom: 0,
+            }
+        },
+        game: {
+            oceanColor: "#0D2B4F",
+            tileLayerOpts: {
+                maxZoom: 7,
+                minZoom: 0,
+            }
+        },
+    }
+}
+
+L.tileLayer('https://s.rsg.sc/sc/images/games/GTAV/map/game/{z}/{x}/{y}.jpg', {
+    maxZoom: 7,
+    minZoom: 0,
+    noWrap: true,
+    attribution: '&copy; Rockstar Games'
+}).addTo(map);
+
+/*
+    https://gtalens.com/assets/images/f2b13a.svg
+    https://gtalens.com/assets/images/d12003.svg
+    https://gtalens.com/assets/images/7171c7.svg
+    https://gtalens.com/assets/images/143e45.svg
+    https://gtalens.com/assets/images/968d80.svg
+    https://gtalens.com/assets/images/a5eaeb.svg
+    https://gtalens.com/assets/images/b29adb.svg
+    https://gtalens.com/assets/images/1466c4.svg
+    https://gtalens.com/assets/images/8c3883.svg
+    https://gtalens.com/assets/images/a4f19c.svg
+*/
