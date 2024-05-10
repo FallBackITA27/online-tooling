43ced9ee6d3828865ab005d6546831f61d86cb75
FallBackITA27 <diegomassanova4@gmail.com>
2024-05-10 16:00:40 +0200

    started work on figurines

--- gtav-interactive-map/script.js
+++ gtav-interactive-map/script.js
@@ -5,6 +5,15 @@ var map = L.map('map', {
     worldCopyJump: false,
 });
 
+const constantData = {
+    icons: {
+        figurine: L.icon({
+            iconUrl: 'icons/figurine.svg',
+            iconSize: [22,22]
+        }),
+    }
+}
+
 L.tileLayer('https://s.rsg.sc/sc/images/games/GTAV/map/game/{z}/{x}/{y}.jpg', {
     maxZoom: 7,
     minZoom: 0,
@@ -19,12 +28,127 @@ async function addSVGOverlay(url, coords) {
     L.svgOverlay(svg, coords).addTo(map);
 }
 
+async function registerMarker(icon, coordinates, title = "",) {
+    L.marker(coordinates, { icon: icon, title: title }).addTo(map);
+}
+
+map.on('click', async function(e) {
+    alert(e.latlng);
+});
+
 addSVGOverlay('overlayedMapItems/kortzCenter.svg', [[20.6, -145], [14.8, -139.7]]);
 addSVGOverlay('overlayedMapItems/prisonBlaineCounty.svg', [[55.9, -67], [50.9, -59.35]]);
 addSVGOverlay('overlayedMapItems/ship.svg', [[-41.43, -72.9], [-44.05, -71.9]]);
 addSVGOverlay('overlayedMapItems/vespucciBeachSewer.svg', [[-1.755, -132.3], [-9.7, -121.755]]);
 L.imageOverlay('overlayedMapItems/fortZancudo.svg', [[63.5, -154.7], [55.1, -128.5]]).addTo(map); // I don't get this bs.
 
+let i = 0;
+for (let coords of [
+    [-42.3, -115.6], // Inside the Building
+    [-28.526622, -115.037842], // Behind the Sign
+    [-35.826721, -98.305664], // Inside the Building
+    [-44.5, -94.163818], // Inside the Building
+    [-44.816916, -87.51709], // Inside the Building
+    [-36.633162, -72.322998], // Inside the Building
+    [-29.238477, -67.236328], // Inside the Building
+    [-29.878755, -79.705811],
+    [-28.921631, -89.846191], // On the bench of the monument
+    [-22.43134, -98.173828], // In the supermarket's backroom
+    [-16.772987, -98.470459], // In the doghouse
+    [-17.947381, -89.79126], // On the washbasin in the building
+    [-12.103781, -93.823242], // Under the bridge
+    [-3.283114, -87.945557], // On the floor in between the tied up carpets
+    [-8.928487, -84.089355], // Inside the security's cubicle 
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+    [0,0],
+]) {
+    i++;
+    registerMarker(constantData.icons.figurine, coords, `Action Figure N. ${i}`);
+}
+i = 0;
 
 let tileLayerData = {
     mainMap: {
