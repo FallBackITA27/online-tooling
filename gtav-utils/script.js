28f22c95f5070d7f900aabcbad75e14e0b5b8c9a
FallBackITA27 <diegomassanova4@gmail.com>
2024-05-10 13:24:22 +0200

    desperate test

--- gtav-utils/script.js
+++ gtav-utils/script.js
@@ -21,7 +21,7 @@ async function addSVGOverlay(url, coords) {
 
 addSVGOverlay('overlayedMapItems/fortZancudo.svg', [[63.5, -154.7], [55.1, -128.5]]);
 addSVGOverlay('overlayedMapItems/kortzCenter.svg', [[20.6, -145], [14.8, -139.7]]);
-addSVGOverlay('overlayedMapItems/prison.svg', [[55.9, -67], [50.9, -59.35]]);
+addSVGOverlay('overlayedMapItems/prisonBlaineCounty.svg', [[55.9, -67], [50.9, -59.35]]);
 addSVGOverlay('overlayedMapItems/ship.svg', [[-41.43, -72.9], [-44.05, -71.9]]);
 addSVGOverlay('overlayedMapItems/vespucciBeachSewer.svg', [[-1.755, -132.3], [-9.7, -121.755]]);
 
