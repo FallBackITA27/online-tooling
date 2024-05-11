7cc8886240ec7a9f1359e293bc18b8ff04399874
FallBackITA27 <diegomassanova4@gmail.com>
2024-05-11 09:32:04 +0200

    FINISHED ALL THE MARKERS

--- gtav-interactive-map/script.js
+++ gtav-interactive-map/script.js
@@ -97,55 +97,55 @@ for (let coords of [
     [33.220308, -68.928223], // On the Kitchen area in front of the pool in Madrazo's backyard
     [11.910354, -63.885498], // Inside the dam's restricted area
     [4.171115, -47.186279], // On the entrance's sign
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
-    [0,0],
+    [19.425154, -46.230469], // In the supermarket's backroom
+    [41.672912, -44.813232], // Inside the train control cubicle
+    [48.268569, -69.093018], // In the house's doghouse
+    [48.319734, -80.233154], // On the shelves with the boxes outside the building
+    [45.228481, -98.371582], // Next to the trash bin
+    [40.178873, -106.116943], // On the counter of the juice stand
+    [47.294134, -109.291992], // To the right of the mine's entrance
+    [39.027719, -127.463379], // On the wooden pier, inside a small lifeboat<sup>(?)</sup>
+    [12.790375, -157.554932], // On the counter for the pool's bar
+    [19.331878, -156.467285], // In the supermarket's backroom
+    [30.391830, -162.070313], // In the trash bin next to the ATM
+    [50.269272, -148.353412], // On the freezer
+    [46.634351, -135.087891], // On the pot next to the church entrance
+    [53.225768, -122.772217], // Behind the monument's inscription
+    [56.583692, -103.073730], // On the chopped tree trunk
+    [53.429174, -89.450684], // On the camper's back side
+    [54.239551, -73.443604], // Inside the LS Customs
+    [54.914514, -60.238037], // Inside the tower
+    [61.159138, -63.171387], // Inside the tower
+    [66.652977, -59.414063], // Behind the glass door
+    [64.081805, -69.345703], // In the backroom of Chef's Supermarket
+    [59.051856, -86.418457], // On the rusty boat's backtail
+    [64.727262, -95.910645], // On the toilet in the middle of the walls
+    [58.418977, -130.155029], // Inside the random carton trash pile
+    [62.349609, -138.043213], // On whatever that thing is called
+    [69.150831, -140.932617], // Right outside the portapotties
+    [74.94513, -140.625], // On the wood pile outside the hut
+    [77.892647, -110.961914], // On the foot of the statue
+    [75.628632, -108.248291], // On the turnings inside the factory
+    [73.763497, -119.597168], // Inside the small Mount Chiliad hole
+    [72.238865, -102.139893], // Right in front of the voltage door inside the tunnel
+    [76.780655, -88.066406], // At the top of the Ski lift
+    [79.775315, -103.40332], // Inside the random carton trash pile
+    [80.05046, -93.647461], // Next to the carton boxes
+    [80.559141, -70.532227], // Inside the small drainage pipe
+    [79.804527, -66.401367], // On the couch outside the camper
+    [76.970245, -52.69043], // On the couch in front of the trespassers sign/confederate flag
+    [72.685765, -62.973633], // Past the glass (You have to break it to pass it)
+    [69.553715, -71.268311], // Inside the hung tire
+    [73.846230, -48.889160], // In between the farm's ajar door
+    [74.930855, -31.113281], // Inside the doghouse
+    [70.685421, -21.159668], // On the wooden stake
+    [71.649833, -38.441162], // Right besides the parked train carriage
+    [68.403224, -42.659912], // Besides the gnome
+    [65.275094, -47.526855], // On the hood of the car with the ufo sculture on it
+    [65.357677, -26.949463], // On the Humane Labs sign
+    [57.521723, -44.439697], // Inside the train control cabin
+    [58.898971, -49.207764], // Spawns only after collecting 98 Action Figures, atop the train parts
+    [58.893296, -49.361572], // Spawns only after collecting 98 Action Figures, on the open plane door
 ]) {
     i++;
     registerMarker(constantData.icons.figurine, coords, `Action Figure N. ${i}`);
